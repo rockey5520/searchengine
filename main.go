ab74eb89ca5e86f8d479918c2ea9bd9ad4d5aab2
rockey5520@gmail.com <rockey5520@gmail.com>
2019-09-26 22:29:30 +0100

    created go routines and its management to show results when routine is finished

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
-package searchengine
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	_ "searchengine/matchers"
+	 "searchengine/search"
+
+)
+
+//init is called prior to the main
+func init() {
+	// change the device for logging to stdout
+	log.SetOutput(os.Stdout)
+}
+
+
+func main() {
+	//perform search for the specified term
+	search.Run("president")
+}
--- search/search.go
+++ search/search.go
@@ -1,1 +1,63 @@
 package search
+
+import (
+	"log"
+	"sync"
+)
+
+// Map of registered matchers for searching
+
+var matchers = make(map[string]Matcher)
+
+// search logic
+
+func Run(searchTerm string) {
+
+	// Retrieve list of feeds to search through
+	feeds, err := RetrieveFeeds()
+	if (err != nil) {
+		log.Fatal(err)
+	}
+
+	// Create an unbuffered channel to receive match results
+	results := make(chan *Result)
+
+	// create wait group so as to process all feeds
+	var waitGroup sync.WaitGroup
+
+	// set the number of goroutines we need to wait for while they process individual feed
+	waitGroup.Add(len(feeds))
+
+	// launch go routine for each feed to find their results
+	for _, feed := range feeds {
+		// Retrieve a matcher for the search
+		matcher, exists := matchers[feed.Type]
+
+		if !exists {
+			matcher = matchers["default"]
+		}
+
+		// launch go routine to perform the search
+		go func(matcher Matcher, feed *Feed) {
+			Match(matcher, feed, searchTerm, results)
+			waitGroup.Done()
+		}(matcher, feed)
+
+	}
+
+	// launch goroutine to monitor when all work is done
+	go func() {
+		// wait for everything is processed
+		waitGroup.Wait()
+		// close channel to signal the display
+		// functin that we exit the program
+		close(results)
+
+	}
+	{
+	}
+
+	// start displaying the results as they are available and return after the final result is displayed
+	Display(resutls)
+
+}
